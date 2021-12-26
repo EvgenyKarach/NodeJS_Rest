8815bc508106938951b864dfd46ee1bd79712608
EvgenyKarach <rok89892@gmail.com>
2021-12-26 16:52:07 +0300

    Create exam model

---
+++ src/resources/exam/exam.service.js
@@ -0,0 +1,22 @@
+const ExamsRepo = require('./exam.memory.repository');
+
+
+const getAll = () => ExamsRepo.getAll();
+
+const getById = (id) => ExamsRepo.getById(id);
+
+
+const createExam = ({abiturientID, teacherID, date, score }) =>
+  ExamsRepo.createExam({ abiturientID, teacherID, date, score });
+
+const deleteById = async (id) => {
+  const examDeletable = await getById(id);
+  ExamsRepo.deleteById(id);
+
+  return examDeletable;
+};
+
+const updateById = ({ id, abiturientID, teacherID, date, score }) =>
+  ExamsRepo.updateById({ id, abiturientID, teacherID, date, score });
+
+module.exports = { getAll, getById, createExam, deleteById, updateById, getTeachersByExamId };
